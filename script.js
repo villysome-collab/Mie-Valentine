f856d996b76fbe348a204274864c514e2745a0e6
villysome-collab <villysome@gmail.com>
2026-02-01 17:39:08 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+const message = document.getElementById("message");
+
+yesBtn.addEventListener("click", () => {
+  message.innerText = "Hurray! You are now Amit's GF 💖🥰";
+});
+
+// No button prank 😈
+noBtn.addEventListener("mouseenter", () => {
+  const x = Math.random() * 300 - 150;
+  const y = Math.random() * 300 - 150;
+  noBtn.style.transform = `translate(${x}px, ${y}px)`;
+});
